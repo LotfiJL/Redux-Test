5133acff032e3e14b666b4ad1c6b263043cf80d3
Lotfi Jellali <lotfijellali40@gmail.com>
2024-01-31 20:50:16 +0100

    mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <ItemCoontainer cake />
-      <CakeContainer />
+      <CakeContainer IceCream />
       <HooksCakecontainer />
       -----------------
       <IceCreamContainer />
