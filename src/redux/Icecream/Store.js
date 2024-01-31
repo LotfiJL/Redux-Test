992d7f47391d6533128cf617e8a0e51350af61b6
Lotfi Jellali <lotfijellali40@gmail.com>
2024-01-31 06:17:39 +0100

    DEVtools testing

--- src/redux/Icecream/Store.js
+++ src/redux/Icecream/Store.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "../rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
-export const store = createStore(rootReducer, applyMiddleware(logger));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
